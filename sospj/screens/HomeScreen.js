c3cca1136aed2029f7d7b0e6072df278351345e0
lsy000428 <lsy000428@naver.com>
2024-03-13 18:59:51 +0900

    test

--- sospj/screens/HomeScreen.js
+++ sospj/screens/HomeScreen.js
@@ -41,7 +41,7 @@ function HomeScreen({ navigation }) {
     </Pressable>
       </View>
       <View style={styles.row}>
-        <Text style={styles.label}>이름</Text>
+        <Text style={styles.label}>이름이</Text>
         <Text style={styles.value}>: 이상용</Text>
       </View>
       <View style={styles.row}>
