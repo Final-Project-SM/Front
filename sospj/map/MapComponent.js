5d1e3e16805e37df89919ba02b71e68d0fc8d11f
lsy000428 <lsy000428@naver.com>
2024-03-25 20:12:44 +0900

    chore : Map에 Kakao 키워드 검색으로 띄우기 - 실패

--- sospj/map/MapComponent.js
+++ sospj/map/MapComponent.js
@@ -1,44 +1,48 @@
 import React from 'react';
 import { StyleSheet, Dimensions } from 'react-native';
 import { WebView } from 'react-native-webview';
-
+import { REACT_APP_KAKKO_KEY } from '@env'; // react-native-dotenv를 통해 환경 변수 불러오기
 const MapComponent = () => {
+  // 마커를 추가하는 JavaScript 코드를 생성하는 함수
+  const createMarkerScript = (position, imageUrl, imageSize) => {
+    return `
+      var markerPosition = new kakao.maps.LatLng(${position.lat}, ${position.lng});
+      var markerImage = new kakao.maps.MarkerImage('${imageUrl}', new kakao.maps.Size(${imageSize.width}, ${imageSize.height}));
+      var marker = new kakao.maps.Marker({
+        position: markerPosition,
+        image: markerImage
+      });
+      marker.setMap(map);
+    `;
+  };
+
   const mapHtml = `
     <html>
       <head>
         <title>Kakao Maps in React Native</title>
-        <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=cd31cc1688cfd0bc17ba44d19c5e80a9&libraries=services"></script>
-      </head>
+        <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=${REACT_APP_KAKKO_KEY}&libraries=services"></script>
+      </head> 
       <body>
         <div id="map" style="width:100%;height:100%;"></div>
         <script>
           var mapContainer = document.getElementById('map'),
               mapOption = {
-                  center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-                  level: 3 // 지도의 확대 레벨
+                  center: new kakao.maps.LatLng(37.566826, 126.9786567),
+                  level: 3
               };  
 
           var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-          // 서울특별시청 위치에 노란색 마커를 추가합니다
-          var cityHallPosition = new kakao.maps.LatLng(37.5662952, 126.9779451);
-          var cityHallMarker = new kakao.maps.Marker({
-              position: cityHallPosition,
-              map: map,
-              // 노란색 마커 이미지 사용
-              image: new kakao.maps.MarkerImage(
-                  'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
-                  new kakao.maps.Size(64, 69)
-              )
-          });
           // 장소 검색 객체를 생성합니다
           var ps = new kakao.maps.services.Places(); 
 
           // 키워드로 장소를 검색합니다
-          ps.keywordSearch('경찰서', placesSearchCB, {location: map.getCenter()}); 
-
-          // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
+          ps.keywordSearch('경찰서', placesSearchCB); 
+          // 키워드 검색 완료 시 호출되는 콜백함수 입니다
           function placesSearchCB (data, status, pagination) {
               if (status === kakao.maps.services.Status.OK) {
+
+                  // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+                  // LatLngBounds 객체에 좌표를 추가합니다
                   var bounds = new kakao.maps.LatLngBounds();
 
                   for (var i=0; i<data.length; i++) {
@@ -46,17 +50,12 @@ const MapComponent = () => {
                       bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
                   }       
 
+                  // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
                   map.setBounds(bounds);
               } 
           }
-
-          // 지도에 마커를 표시하는 함수입니다
-          function displayMarker(place) {
-              var marker = new kakao.maps.Marker({
-                  map: map,
-                  position: new kakao.maps.LatLng(place.y, place.x) 
-              });
-          }
+          // 마커를 추가하는 코드를 여기에 삽입합니다
+          ${createMarkerScript({lat: 37.5662952, lng: 126.9779451}, 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', {width: 64, height: 69})}
         </script>
       </body>
     </html>
